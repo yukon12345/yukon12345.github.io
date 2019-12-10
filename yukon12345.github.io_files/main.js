dde5d633de6dde42dedf6fd0b6562cbc9fb5040d
yukon12345 <yukon12345@163.com>
2019-12-10 11:09:50 +0800

    完成网页构建

--- yukon12345.github.io_files/main.js
+++ yukon12345.github.io_files/main.js
@@ -1,4 +1,4 @@
-var sectionHeight = function() {
+function sectionHeight() {
   var total    = $(window).height(),
       $section = $('section').css('height','auto');
 
@@ -13,17 +13,17 @@ var sectionHeight = function() {
 $(window).resize(sectionHeight);
 
 $(function() {
-  $("section h1, section h2, section h3").each(function(){
-    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
-    $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
-    $("nav ul li:first-child a").parent().addClass("active");
+  $("section h1, section h2, section h3, section h4").each(function(){
+    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().replace(/ /g, '-') + "'>" + $(this).text().replace(/ /g, '-') + "</a></li>");
+      $(this).attr("id",$(this).text().replace(/ /g, '-'));
+    $("nav ul li:first-child a").addClass("active");
   });
 
   $("nav ul li").on("click", "a", function(event) {
     var position = $($(this).attr("href")).offset().top - 190;
     $("html, body").animate({scrollTop: position}, 400);
-    $("nav ul li a").parent().removeClass("active");
-    $(this).parent().addClass("active");
+    $("nav ul li a").removeClass("active");
+    $(this).addClass("active");
     event.preventDefault();
   });
 
